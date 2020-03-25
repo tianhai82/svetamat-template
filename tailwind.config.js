5c049fdb2c746ac60e3ce7188f65b0a160eccde4
Tan Yin Loo <tanyinloo@mrc-asia.com>
2020-03-25 11:08:43 +0800

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   },
   variants: {
     boxShadow: ['responsive', 'hover', 'focus', 'active'],
+    borderRadius: ['responsive', 'hover'],
+    height: ['responsive', 'hover'],
+    width: ['responsive', 'hover'],
   },
   plugins: [
     require('tailwindcss-elevation')(['responsive', 'hover', 'active']),
