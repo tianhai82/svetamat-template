d89d452f9e3c06f8a3518e2b72973be76bf570df
Tan Yin Loo <tanyinloo@mrc-asia.com>
2020-03-30 20:07:38 +0800

    add css in postcss.config.js to exclude from purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,6 @@ module.exports = {
   },
   variants: {
     boxShadow: ['responsive', 'hover', 'focus', 'active'],
-    borderRadius: ['responsive', 'hover'],
-    height: ['responsive', 'hover'],
-    width: ['responsive', 'hover'],
   },
   plugins: [
     require('tailwindcss-elevation')(['responsive', 'hover', 'active']),
