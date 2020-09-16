3780f48985faf5ef87268331a6b3fef93e57f207
Tan Yin Loo <tanyinloo@mrc-asia.com>
2020-09-16 15:12:56 +0800

    fix ts

--- rollup.config.js
+++ rollup.config.js
@@ -29,7 +29,7 @@ export default {
 			}
 		}),
 
-		typescript({ sourceMap: !production }),
+		typescript({ sourceMap: !production, inlineSources: !production }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
